77bfc358591c8dc2ba2b6c58e55fdaf6aee145b9
vkEnine <vkumar@enine.dev>
2024-05-06 12:11:07 +0530

    Task 33

--- tournament/admin.py
+++ tournament/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from models import Team,Match
+from models import Team,Match,TeamMatch
 
 # Register your models here.
 
 admin.site.register(Team)
 admin.site.register(Match)
+admin.site.register(TeamMatch)

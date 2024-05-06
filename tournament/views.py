acbc225461aee2c47223739586cbc2bc79235acd
vkEnine <vkumar@enine.dev>
2024-05-06 11:46:55 +0530

    Task 21

--- tournament/views.py
+++ tournament/views.py
@@ -4,11 +4,13 @@ from.models import Team
 
 # Create your views here.
 class AllTeamView(generic.ListView):
-    
+    template_name = "team_list.html"
     def get_queryset(self):
         return Team.objects.all()
     
 
 class DetailTeamView(generic.DetailView):
     model = Team
+    
+    
 

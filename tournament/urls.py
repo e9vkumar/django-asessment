a8a3b681eb044c3491ebb4ef5c6ecc1d66897bd9
vkEnine <vkumar@enine.dev>
2024-05-06 11:55:04 +0530

    Task 24

--- tournament/urls.py
+++ tournament/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 import views
 urlpatterns = [
     path('/teams/',views.AllTeamView.as_view(),name="all-view"),
-    
+    path('/teams/<team_name>/',views.DetailTeamView.as_view(),name="detail-view"),
 ]

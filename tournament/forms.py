74cb5d2db7580402775e3d56c8123153aed6b752
vkEnine <vkumar@enine.dev>
2024-05-06 11:23:05 +0530

    Task 18

--- tournament/forms.py
+++ tournament/forms.py
@@ -1,9 +1,13 @@
 from django.forms import forms
-from .models import Match
+from .models import Match,TeamMatch
 
 class MatchForm(forms.Form):
     class Meta:
         fields = "__all__"
         model = Match
 
+class TeamMatchForm(forms.Form):
+    class Meta:
+        fields="__all__"
+        model = TeamMatch
 
